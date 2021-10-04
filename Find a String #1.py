1c64ca5a2decb1ff8d5cf567afdb262bedeaaafa
aryabagde <akruyhau@gmail.com>
2021-10-04 13:09:49 +0530

    Find a String #1 #Python

---
+++ Find a String #1.py
@@ -0,0 +1,8 @@
+"""After looking at question we can see that we need 5 iterations for finding a match between a substring(length 3) and string(length 7) 
+"""
+def count_substring(string, sub_string):
+    whole = 0
+    for i in range(len(string) - len(sub_string) + 1):
+        if(string[i:i+len(sub_string)] == sub_string):
+            whole = whole+1;
+    return whole;  
